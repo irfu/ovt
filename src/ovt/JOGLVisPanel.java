ad78ae5ef484ea19eef45162abc9c9dbdb0d552b
Yuri Khotyaintsev <yuri@irfu.se>
2015-02-23 00:06:07 +0100

    new files

---
+++ src/ovt/JOGLVisPanel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ovt;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Iterator;
+import ovt.event.CameraEvent;
+import ovt.interfaces.CameraChangeListener;
+import vtk.rendering.jogl.vtkJoglCanvasComponent;
+import vtk.vtkLight;
+import vtk.vtkRenderWindow;
+import vtk.vtkRenderer;
+
+/**
+ *
+ * @author yuri
+ */
+public class JOGLVisPanel extends vtkJoglCanvasComponent implements ovt.interfaces.RenPanel {
+  
+  private final ovt.CameraChangeSupport cameraChangeSupport;
+  public JOGLVisPanel() {
+    super();
+    this.cameraChangeSupport = new CameraChangeSupport (this);
+  }
+
+  @Override
+  public vtkLight getCameraLight() {
+    return null;
+  }
+
+  public int getWidth() {
+    return this.getComponent().getWidth();
+  }
+
+  public int getHeight() {
+    return this.getComponent().getHeight();
+  }
+
+  public Dimension getSize() {
+    return this.getComponent().getSize();
+  }
+
+  
+  public void fireCameraChange(CameraEvent evt) {
+    cameraChangeSupport.fireCameraChange(evt);
+  }
+
+  @Override
+  public void addCameraChangeListener(CameraChangeListener l) {
+    cameraChangeSupport.addCameraChangeListener(l);
+  }
+
+  public void removeCameraChangeListener(CameraChangeListener l) {
+    cameraChangeSupport.removeCameraChangeListener(l);
+  }
+  
+}
